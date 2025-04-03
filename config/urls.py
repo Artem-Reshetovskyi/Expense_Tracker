c7208ba786e6f9482d7178488624733e760dd623
Arte_Reshetovskyi <Sauronich.d@gmail.com>
2025-04-03 12:34:34 +0200

    add 4 language

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls.i18n import set_language
 from django.urls import path, include
 
 urlpatterns = [
