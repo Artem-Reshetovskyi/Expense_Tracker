66b5fc6dc9eb0f4b6d1b1c7f4e511773c3fca7e6
Arte_Reshetovskyi <Sauronich.d@gmail.com>
2025-04-04 11:55:24 +0200

    fix translate

--- expenses/models.py
+++ expenses/models.py
@@ -3,16 +3,16 @@ from django.utils.translation import gettext_lazy as _
 
 class Expense(models.Model):
     CATEGORY_CHOICES = [
-        ("food", "Food"),
-        ("transport", "Transport"),
-        ("entertainment", "Entertainment"),
-        ("bills", "Bills"),
-        ("other", "Other"),
+        ("food", _("Food")),
+        ("transport", _("Transport")),
+        ("entertainment", _("Entertainment")),
+        ("utilities", _("Utilities")),
+        ("other", _("Other")),
     ]
     name = models.CharField(max_length=255, verbose_name=_("Expense Name"))  # Назва витрати
-    amount = models.DecimalField(max_digits=10, decimal_places=2)  # Сума
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)  # Категорія
-    date = models.DateField()  # Дата створення
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("Amount"))  # Сума
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, verbose_name=_("Category"))  # Категорія
+    date = models.DateField(verbose_name=_("Date"))  # Дата створення
 
     def __str__(self):
-        return f"{self.name} - {self.amount} USD"
+        return f"{self.name} - {self.amount}"
