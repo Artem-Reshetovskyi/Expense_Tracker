85ded6bf09af9a0ea6571ba8038896357f58d3c9
Exhil95 <d.kuczka@gmail.com>
2025-04-06 10:38:55 +0200

    stworzenie testu dla modelu expense

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Expense
+from datetime import date
 
-# Create your tests here.
+
+class ExpenseModelTest(TestCase):
+    """
+    Test dla modelu Expense.
+    """
+    def test_expense_str(self):
+        expense = Expense.objects.create(
+            name="Kebsik",
+            amount=25.50,
+            category="food",
+            date=date.today()
+        )
+        self.assertEqual(str(expense), "Kebsik - 25.5")
+
+
