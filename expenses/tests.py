5a905a2b692546d14b1758bbe2501784aeb99069
Exhil95 <d.kuczka@gmail.com>
2025-04-06 10:41:28 +0200

    test walidacji formularza

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import Expense
 from datetime import date
+from .forms import ExpenseForm
 
 
 class ExpenseModelTest(TestCase):
@@ -17,3 +18,16 @@ class ExpenseModelTest(TestCase):
         self.assertEqual(str(expense), "Kebsik - 25.5")
 
 
+class ExpenseFormTest(TestCase):
+    """
+    Test walidacji dla formularza ExpenseForm.
+    """
+    def test_valid_form(self):
+        form_data = {
+            "name": "Bilet na tramwaj",
+            "amount": 3.50,
+            "category": "transport",
+            "date": "2025-06-04",
+        }
+        form = ExpenseForm(data=form_data)
+        self.assertTrue(form.is_valid())
