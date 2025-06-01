1760ef4c4ca4cf6acde73cb064b8c90c157fdaa4
Arte_Reshetovskyi <Sauronich.d@gmail.com>
2025-06-01 05:19:44 +0200

    fix #2 language

--- expenses/views.py
+++ expenses/views.py
@@ -7,8 +7,8 @@ from django.db.models import Q
 from datetime import datetime
 from django.contrib.auth.decorators import login_required # Декоратор для захисту від неавторизованого доступу  
 
-#Expense - це витрати, які користувач може додавати, редагувати та видаляти.
-    # Відображення списку витрат з можливістю фільтрації та сортування
+# Expense - це витрати, які користувач може додавати, редагувати та видаляти.
+# Відображення списку витрат з можливістю фільтрації та сортування
 @login_required 
 def expense_list(request):
     expenses = Expense.objects.all()  # За замовчуванням беремо всі витрати
@@ -87,7 +87,29 @@ def delete_all_expenses(request):
 @login_required
 def income_list(request):
     incomes = Income.objects.filter(user=request.user)
+   
+    # Фільтрація за описом
+    description = request.GET.get("description")
+    if description:
+        incomes = incomes.filter(description=description)
+
+    # Фільтрація за датою
+    date_from = request.GET.get("date_from", "")
+    date_to = request.GET.get("date_to", "")
+    if date_from:
+        incomes = incomes.filter(date__gte=datetime.strptime(date_from, "%Y-%m-%d"))
+    if date_to:
+        incomes = incomes.filter(date__lte=datetime.strptime(date_to, "%Y-%m-%d"))
+
+    # Сортування
+    sort_by = request.GET.get("sort_by", "")
+    if sort_by == "amount":
+        incomes = incomes.order_by("amount")
+    elif sort_by == "date":
+        incomes = incomes.order_by("date")
+        
     return render(request, "incomes/income_list.html", {"incomes": incomes})
+
 
 
 @login_required
